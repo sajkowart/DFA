d393b777b1f47af0768cfbd63999bda120e6ff5f
sajkowart <sajkowart@gmail.com>
2021-01-30 04:51:47 +0100

    b4 form2 designer

--- DFA/Form2.cs
+++ DFA/Form2.cs
@@ -24,7 +24,7 @@ namespace DFA
         public static extern bool RegisterRawInputDevices(RAWINPUTDEVICE pRawInputDevices, uint uiNumDevices, uint cbSize);
 
         public IntPtr HWnd { get; set; }
-
+        
 
         private const int WM_NCHITTEST = 0x84;
         private const int HTCLIENT = 0x1;
@@ -43,6 +43,7 @@ namespace DFA
         public const int WM_NCLBUTTONDOWN = 0xA1;
         private Label label4;
         private Label label5;
+        private ProgressBar mainProgressBar;
         public const int HT_CAPTION = 0x2;
 
         [DllImportAttribute("user32.dll")]
@@ -140,10 +141,58 @@ namespace DFA
             this.Location = new Point(DefaultWindowX, DefaultWindowY);
         }
 
+        private NotifyIcon trayIcon;
+
+        public void CreateTrayMenu()
+        {
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = new System.Drawing.Icon("Resources/AppIcon.ico");
+            trayIcon.Text = "DFA";
+            trayIcon.Click += TrayIconClicked;
+
+
+            trayIcon.ContextMenuStrip = new ContextMenuStrip();
+            trayIcon.ContextMenuStrip.Items.Add("Exit", Image.FromFile("Resources/AppIcon.ico"), OnExitClicked);
+            //label button
+            trayIcon.ContextMenuStrip.Items.Add(new ToolStripDropDownButton("Settings...", null,
+                new ToolStripLabel("Reset position",null,false,TrayResetPosition),
+                new ToolStripLabel("test2")));
+
+
+
+
+
+
+            trayIcon.Visible = true;
+
+
+
+
+        }
+
+        private void TrayResetPosition(object sender, EventArgs e)
+        {
+            ResetWindowPosition();
+        }
+
+        private void OnExitClicked(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void TrayIconClicked(object sender, EventArgs e)
+        {
+           // MessageBox.Show("cl");
+        }
+
         public Form2()
         {
             
             InitializeComponent();
+            CreateTrayMenu();
+
             LoadWindowPosition();
 
             this.TopMost = true;
@@ -264,19 +313,28 @@ namespace DFA
                 message.Result = (IntPtr)HTCAPTION;
         }
 
-        private void label2_Click(object sender, EventArgs e)
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
         {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+
+
+
+            base.OnFormClosed(e);
 
         }
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.mainProgressBar = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -286,7 +344,7 @@ namespace DFA
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1104, 30);
+            this.pictureBox1.Size = new System.Drawing.Size(984, 31);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -296,7 +354,7 @@ namespace DFA
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(143)))), ((int)(((byte)(206)))));
-            this.label1.Location = new System.Drawing.Point(519, 0);
+            this.label1.Location = new System.Drawing.Point(185, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 1;
@@ -307,7 +365,7 @@ namespace DFA
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(143)))), ((int)(((byte)(206)))));
-            this.label2.Location = new System.Drawing.Point(662, 0);
+            this.label2.Location = new System.Drawing.Point(328, 6);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 2;
@@ -318,7 +376,7 @@ namespace DFA
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(143)))), ((int)(((byte)(206)))));
-            this.label3.Location = new System.Drawing.Point(791, 0);
+            this.label3.Location = new System.Drawing.Point(457, 6);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 15);
             this.label3.TabIndex = 3;
@@ -329,7 +387,7 @@ namespace DFA
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(143)))), ((int)(((byte)(206)))));
-            this.label4.Location = new System.Drawing.Point(910, 0);
+            this.label4.Location = new System.Drawing.Point(576, 6);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 15);
             this.label4.TabIndex = 3;
@@ -340,22 +398,44 @@ namespace DFA
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(143)))), ((int)(((byte)(206)))));
-            this.label5.Location = new System.Drawing.Point(998, 0);
+            this.label5.Location = new System.Drawing.Point(664, 6);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(38, 15);
             this.label5.TabIndex = 3;
             this.label5.Text = "label5";
             // 
+            // mainProgressBar
+            // 
+            this.mainProgressBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.mainProgressBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mainProgressBar.ForeColor = System.Drawing.Color.Maroon;
+            this.mainProgressBar.Location = new System.Drawing.Point(0, 0);
+            this.mainProgressBar.Margin = new System.Windows.Forms.Padding(0);
+            this.mainProgressBar.MarqueeAnimationSpeed = 500;
+            this.mainProgressBar.MaximumSize = new System.Drawing.Size(0, 8);
+            this.mainProgressBar.Name = "mainProgressBar";
+            this.mainProgressBar.Size = new System.Drawing.Size(984, 2);
+            this.mainProgressBar.Step = 1;
+            this.mainProgressBar.TabIndex = 4;
+            this.mainProgressBar.Value = 70;
+            // 
             // Form2
             // 
-            this.ClientSize = new System.Drawing.Size(1104, 30);
+            this.ClientSize = new System.Drawing.Size(984, 31);
+            this.Controls.Add(this.mainProgressBar);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form2";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
